8b0ce356efc0756e3e0bd59d40a442b4548c1541
shaheen5 <miyashaheen1995@gmail.com>
2021-04-07 20:56:50 +0530

    Added : Method To Initialize Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,18 @@
 package Tic_Tac_Toe_Problem;
 
 public class TicTacToeGame {
-
+	//declare & allocate memory to character array of size 10
+	static char[]board = new char[10];
+	
+	//initialize board to empty spaces
+	private static void createBoard() {
+		for(int i=1;i<board.length;i++) {
+			board[i]=' ';
+		}
+	}
+		
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
-
+		createBoard();
 	}
-
 }
