bdf36097d698d7c40f41d39bd7e50b5dfb166dda
shaheen5 <miyashaheen1995@gmail.com>
2021-04-07 22:15:54 +0530

    Added : Method To Allow Player To Input 'X' or 'O'

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package Tic_Tac_Toe_Problem;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	//declare & allocate memory to character array of size 10
 	static char[]board = new char[10];
@@ -10,9 +12,22 @@ public class TicTacToeGame {
 			board[i]=' ';
 		}
 	}
-		
+	
+	//get user input character & return it's UpperCase character 
+	private static char getPlayerLetter(Scanner userInput) {
+		System.out.print("Choose your letter :");
+		return userInput.next().toUpperCase().charAt(0);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
 		createBoard();
+		Scanner userInput=new Scanner(System.in);
+		char playerLetter=getPlayerLetter(userInput);
+		while(playerLetter!='X'&& playerLetter!='O') {
+			System.out.println("Invalid Input ! Choose Either 'X' or 'O':-");
+			playerLetter=getPlayerLetter(userInput);
+		}
+		char computerLetter=( playerLetter=='X')?'O': 'X' ;
 	}
 }
